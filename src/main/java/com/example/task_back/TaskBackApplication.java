c0f6540585ed24ac84b4c1ba10d1430867232719
bsko98 <bsko98@naver.com>
2024-09-05 12:01:45 +0900

    프로젝트 생성 및 게시글 등록 화면 구현

---
+++ src/main/java/com/example/task_back/TaskBackApplication.java
@@ -0,0 +1,14 @@
+package com.example.task_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBackApplication.class, args);
+		System.out.println("ㄴㅇㄹㄴㅇㅇㄴㄹ");
+	}
+
+}
---
+++ src/main/java/com/example/task_back/pray/Pary.java
@@ -0,0 +1,14 @@
+package com.example.task_back.pray;
+
+
+/*
+@Entity
+public class Pary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+}
+*/
