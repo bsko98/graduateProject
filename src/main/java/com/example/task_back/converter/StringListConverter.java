c189cddbd3be95e3d51016cd8f7fc795f9a9e5d6
bsko98 <bsko98@naver.com>
2024-10-26 02:04:51 +0900

    기도문을 작성하고 수정할 때 gpt를 이용해 키워드랑 카테고리를 추출해 DB에 저장하고 저장된 값을 불러오게 수정, 현재 달을 기준으로 이번달 기도문들을 가져오게 수정

---
+++ src/main/java/com/example/task_back/converter/StringListConverter.java
@@ -0,0 +1,34 @@
+package com.example.task_back.converter;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to convert list to JSON string", e);
+        }
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, List.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to convert JSON string to list", e);
+        }
+    }
+}
